2194f0ea7f0d031a58b2513a81272d2bbc4d3c32
Chris Yu <yuetong3yu@gmail.com>
2025-01-09 18:51:30 +1100

    update config

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -11,11 +11,11 @@ interface SiteConfig {
 
 export const siteConfig: SiteConfig = {
 	site: 'https://blog-template-gray.vercel.app/', // Write here your website url
-	author: 'Your Name', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
-	lang: 'en-GB',
-	ogLocale: 'en_GB',
+	author: 'Chris Yu', // Site author
+	title: 'Chris Yu', // Site title.
+	description: 'A SWE living in Sydney', // Description to display in the meta tags
+	lang: 'en',
+	ogLocale: 'en',
 	shareMessage: 'Share this post', // Message to share a post on social media
 	paginationSize: 6 // Number of posts per page
 }
